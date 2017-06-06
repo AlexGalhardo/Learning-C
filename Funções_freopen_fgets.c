5e4ef3ac658dd175b617d58c177ec1dfb3acfe5f
Alex Galhardo <aleexgvieira@gmail.com>
2017-06-06 05:58:27 -0300

    Update Funções_freopen_fgets.c

--- Funções_freopen_fgets.c
+++ Funções_freopen_fgets.c
@@ -1,10 +1,10 @@
+/* ---------- parte 1 ------------ */
 #include <stdio.h>
 
 int main(){
-	
 	char x[100];
 	
-	//FILE *file = fopen
+	// stdin = standard input => entrada padrão => no caso do PC, geralmente é o teclado
 	
 	fgets(x, 100, stdin);
 	printf("%s", x);
@@ -12,6 +12,26 @@ int main(){
 	freopen("Arquivo 1.txt", "r", stdin);
 	fgets(x, 100, stdin);
 	printf("%s", x);
+	
+	return 0;
+}
+
+/* ---------- parte 2 ------------ */
+
+#include <stdio.h>
+
+int main(){
+	
+	char x[100];
+	
+	FILE *file = fopen("Arquivo 1.txt", "r");
+	
+	fgets(x, 100, file);
+	printf("%s\n", x);
+	
+	freopen("Arquivo 2.txt", "r", file);
+	fgets(x, 100, file);
+	printf("%s\n", x);
 	
 	return 0;
 }

04256f4e3762248230fd3cd56426deae20f071a1
Alex Galhardo <aleexgvieira@gmail.com>
2017-07-02 17:31:52 -0300

    Add files via upload
    https://www.youtube.com/watch?v=X6BcBhRCR8M&list=PL8iN9FQ7_jt4TfE02fK7gCkYOzL4htaE2&index=2

---
+++ arquivos/FPUTC.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+	FILE *f;
+	f = fopen("arquivo.txt", "w");
+
+	if(f !=NULL){
+		printf("ERRO NA ABERTURA\n");
+		exit(1); // parar o programa
+	}
+
+	char texto[20] = "Meu programa em C";
+
+	int i;
+
+	for(i=0; i<strlen(texto); i++){
+		fputc(texto[i], f);
+	}
+
+	fclose(f);
+
+	return 0;
+}

f2587f59564f2c50df2e57fdb76057a4745b346f
Alex Galhardo <aleexgvieira@gmail.com>
2017-10-20 23:19:29 -0200

    Create BucketSort.c

---
+++ Algoritmos de Ordenacao/RunCodes/BucketSort.c
@@ -0,0 +1,17 @@
+/*
+
+Bucket sort
+Faça uma implementação do algoritmo de ordenação "bucket sort". O algoritmo deverá ler um número N que indica a quantidade de valores a serem ordenados. Em uma outra linha, o algoritmo deverá ler os N valores a serem ordenados, separados par a par por um único espaço. A saída do método deverá ser os valores ordenados ascendentemente e separados par a par por um único espaço. Imprima os números com 4 casos decimais.
+
+É garantido que todos valores a serem ordenados são números reais entre 0 e 1.
+
+Exemplo de Entrada e Saída
+Entrada:
+
+10
+0.5000 0.3000 0.2000 0.7000 0.6000 0.8000 0.9000 0.1000 0.0000 0.4000
+Saída:
+
+0.0000 0.1000 0.2000 0.3000 0.4000 0.5000 0.6000 0.7000 0.8000 0.9000
+
+*/
